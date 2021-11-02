99caa6e90332730efc5597b4afdbaf8c4d62407d
Andrew Kee <andrewjasonkee@gmail.com>
2021-11-01 20:55:32 -0600

    simple method added

--- all.js
+++ all.js
@@ -94,3 +94,168 @@ function editBounds(){
 
   }
 }
+
+
+
+function runSimple(){
+	simpleButton = document.getElementById("simpleButton");
+	advancedButtom = document.getElementById("advancedButton");
+
+	simpleInputsExtem = document.getElementById("extemSimple");
+	simpleInputsFibtem = document.getElementById("fibtemSimple");
+
+	advancedInputsExtem = document.getElementById("extemAdvanced");
+	advancedInputsFibtem = document.getElementById("fibtemAdvanced");
+	advancedAnalyzeButton = document.getElementById("advancedAnalyzeButton");
+
+	if(simpleButton.classList.contains("pressed")){
+		return
+	}
+	else{
+		simpleButton.classList.add("pressed")
+		simpleButton.classList.remove("unpressed")
+		advancedButton.classList.add("unpressed")
+		advancedButton.classList.remove("pressed")
+
+		advancedInputsExtem.style.display = "none";
+		advancedInputsFibtem.style.display = "none";
+		advancedAnalyzeButton.style.display = "none";
+
+		simpleInputsExtem.style.display = "inline";
+		// simpleInputsFibtem.style.display = "inline";
+
+	}
+
+	document.getElementById("output").innerHTML = "";
+}
+
+
+function runAdvanced(){
+	simpleButton = document.getElementById("simpleButton");
+	advancedButtom = document.getElementById("advancedButton");
+
+	simpleInputsExtem = document.getElementById("extemSimple");
+	simpleInputsFibtem = document.getElementById("fibtemSimple");
+
+	advancedInputsExtem = document.getElementById("extemAdvanced");
+	advancedInputsFibtem = document.getElementById("fibtemAdvanced");
+
+	advancedAnalyzeButton = document.getElementById("advancedAnalyzeButton");
+
+	if(advancedButton.classList.contains("pressed")){
+		return
+	}
+	else{
+		simpleButton.classList.remove("pressed");
+		simpleButton.classList.add("unpressed");
+		advancedButton.classList.remove("unpressed");
+		advancedButton.classList.add("pressed");
+
+		advancedInputsExtem.style.display = "inline";
+		advancedInputsFibtem.style.display = "inline";
+		advancedAnalyzeButton.style.display = "inline";
+
+		simpleInputsExtem.style.display = "none";
+
+	}
+	document.getElementById("output").innerHTML = "";
+}
+
+
+
+function runSimpleAnalysis(input_button){
+
+	deets = input_button.split("_")
+	
+	if(deets[0] == "E"){
+		if(deets[1] == "CT"){
+			x = document.getElementsByClassName("E_CT");
+			for (let item of x) {
+			    document.getElementById(item.id).classList.remove("pressed_inputs");
+			}
+		}
+		else if(deets[1] == "Alpha"){
+			x = document.getElementsByClassName("E_Alpha");
+			for (let item of x) {
+			    document.getElementById(item.id).classList.remove("pressed_inputs");
+			}
+		}
+		else if(deets[1] == "MCF"){
+			x = document.getElementsByClassName("E_MCF");
+			for (let item of x) {
+			    document.getElementById(item.id).classList.remove("pressed_inputs")
+			}
+		}
+	}
+	else if(deets[0] == "F"){
+		if(deets[1] == "A10"){
+			x = document.getElementsByClassName("F_A10");
+			for (let item of x) {
+			    document.getElementById(item.id).classList.remove("pressed_inputs");
+			}
+		}
+		else if(deets[1] == "MCF"){
+			x = document.getElementsByClassName("F_MCF");
+			for (let item of x) {
+			    document.getElementById(item.id).classList.remove("pressed_inputs");
+			}
+		}
+	}
+	document.getElementById(input_button).classList.remove("styled");
+	document.getElementById(input_button).classList.add("pressed_inputs");
+
+
+
+
+	recommendation = ""
+	// flowchart
+	// check extem CT
+	// if(extem_CT >= extem_CT_h){
+	if(document.getElementById("E_CT_high").classList.contains("pressed_inputs")){
+		recommendation += "Give FFP <br>";
+	}
+	// if(extem_alpha <= extem_alpha_l){
+	if(document.getElementById("E_Alpha_low").classList.contains("pressed_inputs")){
+		recommendation += "Give Cryoprecipitate <br>";
+	}
+
+	// check etem MCF
+	// if(extem_MCF < extem_MCF_l){
+	if(document.getElementById("E_MCF_low").classList.contains("pressed_inputs")){
+		// check fibtem MCF
+		// if(fibtem_MCF <= fibtem_MCF_l){
+		if(document.getElementById("F_MCF_low").classList.contains("pressed_inputs")){
+			recommendation += "Give Cryoprecipitate <br>";
+		}
+		// if(fibtem_MCF > fibtem_MCF_l){
+		if(document.getElementById("F_MCF_WNL").classList.contains("pressed_inputs") || document.getElementById("F_MCF_high").classList.contains("pressed_inputs")){
+			recommendation += "Give Platelets <br>";
+		}
+
+		// if(fibtem_A10 <= 18){
+		if(document.getElementById("F_A10_WNL").classList.contains("pressed_inputs")){
+			recommendation += "If actively bleeding, Give Cryoprecipitate <br>";
+		}	
+		// if(fibtem_A10 <= 9){
+		if(document.getElementById("F_A10_low").classList.contains("pressed_inputs")){	
+			recommendation +=  "If no active bleeding, give Cryoprecipitate <br>";
+		}
+	}
+	document.getElementById("output").innerHTML = recommendation;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
